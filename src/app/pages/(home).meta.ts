e677d6345fb7b69d04c9b6f1da5928bd95bd38f6
markostanimirovic <markostanimirovic95@gmail.com>
2023-08-29 13:01:58 +0200

    fix: prerender root page

--- src/app/pages/(home).meta.ts
+++ src/app/pages/(home).meta.ts
@@ -2,7 +2,7 @@ import { RouteMeta } from '@analogjs/router';
 
 const title = 'Angular Belgrade Day 2023';
 const description =
-  "Welcome to Angular Belgrade Day 2023! Join us for a day packed with insightful talks. Explore the latest trends, best practices, and real-world use cases of Angular. Whether you're a seasoned pro or just starting with Angular, this event is your chance to learn, connect, and get inspired.";
+  'Welcome to Angular Belgrade Day 2023! Join us on November 4 to explore the latest trends and best practices in the Angular ecosystem.';
 const image = 'https://angularbelgrade.com/banners/angular-belgrade-day-23.jpg';
 
 export const routeMeta: RouteMeta = {
--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig(({ mode }) => ({
         routes: ['/'],
         sitemap: { host: 'https://angularbelgrade.org/' },
       },
+      nitro: { serveStatic: false },
     }),
   ],
   test: {
